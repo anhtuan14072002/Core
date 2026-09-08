d5a9ce654b4141cd94bb987e2ac1d9aa129c79cd
DESKTOP-S7QAM7F\TunaPC <anhtuan14072002@gmail.com>
2026-09-09 00:30:18 +0700

    update

---
+++ com.tuna.ui/Runtime/Text/AutoDotNumberText.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TMP_Text))]
+public class AutoDotNumberText : MonoBehaviour
+{
+    [SerializeField] private bool formatOnEnable = true;
+    [SerializeField] private bool formatEveryFrame = false;
+
+    private TMP_Text _text;
+
+    private static readonly CultureInfo Invariant = CultureInfo.InvariantCulture;
+    private static readonly Regex NumberRegex = new Regex(@"^\d+$"); 
+
+    private void Awake()
+    {
+        _text = GetComponent<TMP_Text>();
+    }
+
+    private void OnEnable()
+    {
+        if (formatOnEnable)
+            FormatIfNumber();
+    }
+
+    private void Update()
+    {
+        if (formatEveryFrame)
+            FormatIfNumber();
+    }
+
+    public void FormatIfNumber()
+    {
+        if (_text == null) return;
+
+        string raw = _text.text.Replace(".", "");
+
+        if (NumberRegex.IsMatch(raw))
+        {
+            if (long.TryParse(raw, out long value))
+            {
+                _text.text = value
+                    .ToString("N0", Invariant)
+                    .Replace(",", ".");
+            }
+        }
+    }
+}
